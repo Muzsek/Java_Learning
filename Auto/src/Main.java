2d09aefc87b2f9374a6f5ab716e489e648d58abc
Muzsek <147140150+Muzsek@users.noreply.github.com>
2025-09-16 14:01:31 +0200

    1 class + main

---
+++ Auto/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Auto auto = new Auto("Toyota","Corola",2015,10100);
+        auto.autoInfo();
+        auto.megtettUt(10000);
+        auto.autoInfo();
+    }
+}
